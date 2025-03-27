38ef999ce26f37dc309b37349f09536648f6adf8
Ladislav Vondráček <lawondyss@gmail.com>
2025-03-27 22:04:31 +0100

    Added positionals arguments to DynamicResult

--- src/Result/DynamicResult.php
+++ src/Result/DynamicResult.php
@@ -4,16 +4,24 @@ namespace Lawondyss\Parex\Result;
 
 use AllowDynamicProperties;
 
+use function is_int;
+
 #[AllowDynamicProperties]
 class DynamicResult extends Result
 {
+  /** @var string[] $POSITIONAL */
+  public array $POSITIONAL = [];
+
+
   /**
-   * @param array<string, string|string[]|null> $args
+   * @param array<array-key, string|string[]|null> $args
    */
   public function __construct(...$args)
   {
     foreach ($args as $name => $value) {
-      $this->{$name} = $value;
+      is_int($name)
+        ? $this->POSITIONAL[] = $value
+        : $this->{$name} = $value;
     }
   }
 }
